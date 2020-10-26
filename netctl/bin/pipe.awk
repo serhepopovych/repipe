serhepopovych/repipe

commit 9d1ee0a07f038d8587acccda975245f7aff50776

repipe: Improve pipe configuration
Rename usrxml.awk to pipe.awk to clearly show it's purpose; make sure
pipes inherited from users.conf in case of no ones provided via shapers
helper.

Process entries directly without callback to run_usrxml_parse() helper
to unify tc.awk from retc subproject and pipe.awk from repipe project.

Be more robust and treat anything except "world" and "all" as "local"
zone. Convert bandwidth from known units to kbit and treat unknown ones
as kbit.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/bin/pipe.awk
@@ -1,0 +1,172 @@
+#!/usr/bin/gawk -f
+
+# Source USRXML database parsing library.
+@include "@target@/netctl/lib/awk/libusrxml.awk"
+
+################################################################################
+
+BEGIN{
+	##
+	## Initialize user database parser.
+	##
+	h = init_usrxml_parser("pipe.awk");
+	if (h < 0)
+		exit 1;
+}
+
+{
+	##
+	## Parse user database.
+	##
+	line = $0;
+	if (run_usrxml_parser(h, line) < 0)
+		exit 1;
+}
+
+END{
+	##
+	## Data-rate units.
+	##
+
+	# SI
+	units["kbit"] = 1000;
+	units["kbps"] = units["kbit"] * 8;
+	units["mbit"] = 1000 * 1000;
+	units["mbps"] = units["mbit"] * 8;
+	units["gbit"] = 1000 * 1000 * 1000;
+	units["gbps"] = units["gbit"] * 8;
+	units["tbit"] = 1000 * 1000 * 1000 * 1000;
+	units["tbps"] = units["tbit"] * 8;
+	# IEC
+	units["kibit"] = 1024;
+	units["kibps"] = units["kibit"] * 8;
+	units["mibit"] = 1024 * 1024;
+	units["mibps"] = units["mibit"] * 8;
+	units["gibit"] = 1024 * 1024 * 1024;
+	units["gibps"] = units["gibit"] * 8;
+	units["tibit"] = 1024 * 1024 * 1024 * 1024;
+	units["tibps"] = units["tibit"] * 8;
+
+	if (fout)
+		print "" >fout;
+
+	prog = USRXML__instance[h,"prog"];
+
+	ifn = USRXML_ifnames[h,"num"];
+	for (iff = 0; iff < ifn; iff++) {
+		# h,userid
+		i = h SUBSEP iff;
+
+		# Skip holes entries
+		if (!(i in USRXML_ifnames))
+			continue;
+
+		username = USRXML_ifnames[i];
+
+		# Skip inactive users
+		if (USRXML_ifnames[h,username,"inactive"])
+			continue;
+
+		# h,"orig"
+		hh = h USRXML_orig;
+
+		# hh,userid
+		i_dst = hh SUBSEP iff;
+
+		usrxml__copy_user_pipe(hh, i_dst, i);
+		usrxml__delete_pipe(i, 1);
+
+		cmd = "@target@/netctl/bin/shapers " username;
+		while ((ret = (cmd | getline line)) > 0) {
+			printf "%s: %s: line: %s\n",
+				prog, username, line >"/dev/stderr";
+
+			n = split(line, a, " ");
+			if (n == 3) {
+				## Valid policy in format: "zone dir bw", apply
+
+				# Translate units to kbit (default is kbit)
+				unit = tolower(a[3]);
+				sub("^[[:digit:]]+", "", unit);
+				sub(unit "$", "", a[3]);
+
+				if (unit in units)
+					a[3] *= units[unit] / units["kbit"];
+
+				# Translate unknown zones to "local"
+				if (a[1] != "world" && a[1] != "all")
+					a[1] = "local";
+
+				p = USRXML_userpipe[i]++;
+
+				# h,userid,pipeid
+				j = i SUBSEP p;
+
+				USRXML_userpipe[j] = p;
+				USRXML_userpipe[j,"zone"] = a[1];
+				USRXML_userpipe[j,"dir"] = a[2];
+				USRXML_userpipe[j,"bw"] = a[3];
+			} else if (n == 0) {
+				## No fields: may be empty line, skip
+			} else if (n == 1 && a[1] == "none") {
+				## Valid policy in format: "none", skip
+			} else {
+				## Unknown format: failure
+				break;
+			}
+		}
+		close(cmd);
+
+		m = (i in USRXML_userpipe) ? USRXML_userpipe[i] : 0;
+
+		if (ret || !m || usrxml__scope_validate_pipe(i))
+			usrxml__copy_user_pipe(h, i, i_dst);
+
+		usrxml__delete_pipe(i_dst, 1);
+
+		if (ret > 0) {
+			printf "%s: %s: get shapers failed\n",
+				prog, username >"/dev/stderr";
+		} else if (ret < 0) {
+			printf "%s: %s: read error: %s\n",
+				prog, username, ERRNO >"/dev/stderr";
+		} else {
+			print_usrxml_entry(h, username, fout);
+		}
+
+		# Report final shaper configuration to stderr
+		ret = -1;
+
+		m = (i in USRXML_userpipe) ? USRXML_userpipe[i] : 0;
+
+		for (p = 0; p < m; p++) {
+			# h,userid,pipeid
+			j = i SUBSEP p;
+
+			# Skip hole entries
+			if (!(j in USRXML_userpipe))
+				continue;
+
+			line = "";
+			line = line USRXML_userpipe[j,"zone"] " ";
+			line = line USRXML_userpipe[j,"dir"] " ";
+			line = line USRXML_userpipe[j,"bw"];
+
+			printf "%s: %s: pipe: %s\n",
+				prog, username, line >"/dev/stderr";
+
+			ret = 1;
+		}
+
+		if (ret < 0) {
+			printf "%s: %s: no shapers\n",
+				prog, username >"/dev/stderr";
+		}
+	}
+
+	##
+	## Finish user database parsing.
+	##
+	if (fini_usrxml_parser(h) < 0)
+		exit 1;
+}

netctl/bin/repipe
@@ -75,8 +75,9 @@ nctl_check_ok 'ERRORS WHEN BACKUP FILES\n'
 nctl_log_msg 'Generating user database file "%s" based on "%s" file\n' \
 	"$USR_XML" "$USERS_CONF"
 gawk \
-	-f "$NCTL_PREFIX/bin/usrxml.awk" \
-	"$USERS_CONF" 2>&1 >"$USR_XML" |nctl_log_pipe
+	-v fout="$USR_XML" \
+	-f "$NCTL_PREFIX/bin/pipe.awk" \
+	"$USERS_CONF" 2>&1 |nctl_log_pipe
 nctl_check_ok 'ERRORS ON GENERATING USER DATABASE FILE USR.XML\n'
 __restore_original_files=n
 

netctl/bin/usrxml.awk
@@ -1,90 +1,0 @@
-#!/usr/bin/gawk -f
-
-# Source USRXML database parsing library.
-@include "@target@/netctl/lib/awk/libusrxml.awk"
-
-################################################################################
-
-function print_usrxml_entry_pipe(h, userid, file,    n, i, j, p, a, cmd, line, ret, errstr)
-{
-	# h,userid
-	i = h SUBSEP userid;
-
-	cmd = "@target@/netctl/bin/shapers " USRXML_users[i];
-	errstr = "";
-
-	# pipe
-	p = 0 + USRXML_userpipe[i];
-
-	while ((ret = (cmd | getline line)) > 0) {
-		n = split(line, a, " ");
-		if (n == 3) {
-			# h,userid,pipeid
-			j = i SUBSEP p;
-
-			## Valid policy in format: "zone dir bw"
-
-			# Skip "Kb" at the end
-			sub("Kb$", "", a[3]);
-
-			USRXML_userpipe[j] = ++p;
-			USRXML_userpipe[j,"zone"] = a[1];
-			USRXML_userpipe[j,"dir"] = a[2];
-			USRXML_userpipe[j,"bw"] = a[3];
-		} else if (n == 0) {
-			## No fields: may be empty line, skip
-			continue;
-		} else if (n == 1 && a[1] == "none") {
-			# Single field: "none"
-			break;
-		} else {
-			## Unknown format: failure
-			errstr = sprintf("%s: shapers unknown format",
-					 USRXML_users[i]);
-			ret = USRXML_E_GETLINE;
-			break;
-		}
-	}
-	close(cmd);
-
-	if (ret < 0) {
-		if (errstr == "") {
-			errstr = sprintf("%s: pipe read error: %s",
-					 USRXML_users[i], ERRNO);
-		}
-		usrxml_result(h, USRXML_E_GETLINE, USRXML_MSG_PRIO_ERR, errstr);
-		usrxml_clearerrno(h);
-	} else {
-		USRXML_userpipe[i] = p;
-		print_usrxml_entry(h, userid, file);
-	}
-
-	# Ignore errors to continue on next entry
-	return userid + 1;
-}
-
-BEGIN{
-	##
-	## Initialize user database parser.
-	##
-	h = init_usrxml_parser("repipe")
-	if (h < 0)
-		exit 1;
-}
-
-{
-	##
-	## Parse user database.
-	##
-	line = $0;
-	if (run_usrxml_parser(h, line, "print_usrxml_entry_pipe") < 0)
-		exit 1;
-}
-
-END{
-	##
-	## Finish user database parsing.
-	##
-	if (fini_usrxml_parser(h) < 0)
-		exit 1;
-}

commit 1674b9d0a2dfea680ad883aafbe5aa0c3f3bbab7

repipe: Strip unit suffix and number pipes correctly
Strip unit suffix before possible modification with tolower(). Otherwise
subsequent sub() will fail and bandwidth value will end with malformed
value (e.g. Kb or KbKb).

Pipes numbering starts with one (1), not zero (0).

Fixes: 9d1ee0a07f03 ("repipe: Improve pipe configuration")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/bin/pipe.awk
@@ -86,9 +86,10 @@ END{
 				## Valid policy in format: "zone dir bw", apply
 
 				# Translate units to kbit (default is kbit)
-				unit = tolower(a[3]);
+				unit = a[3];
 				sub("^[[:digit:]]+", "", unit);
 				sub(unit "$", "", a[3]);
+				unit = toupper(unit);
 
 				if (unit in units)
 					a[3] *= units[unit] / units["kbit"];
@@ -97,7 +98,7 @@ END{
 				if (a[1] != "world" && a[1] != "all")
 					a[1] = "local";
 
-				p = USRXML_userpipe[i]++;
+				p = ++USRXML_userpipe[i];
 
 				# h,userid,pipeid
 				j = i SUBSEP p;