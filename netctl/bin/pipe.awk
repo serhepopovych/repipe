serhepopovych/repipe

commit 1674b9d0a2dfea680ad883aafbe5aa0c3f3bbab7

repipe: Strip unit suffix and number pipes correctly
Strip unit suffix before possible modification with tolower(). Otherwise
subsequent sub() will fail and bandwidth value will end with malformed
value (e.g. Kb or KbKb).

Pipes numbering starts with one (1), not zero (0).

Fixes: 9d1ee0a07f03 ("repipe: Improve pipe configuration")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/bin/pipe.awk
@@ -86,9 +86,10 @@ END{
 				## Valid policy in format: "zone dir bw", apply
 
 				# Translate units to kbit (default is kbit)
-				unit = tolower(a[3]);
+				unit = a[3];
 				sub("^[[:digit:]]+", "", unit);
 				sub(unit "$", "", a[3]);
+				unit = toupper(unit);
 
 				if (unit in units)
 					a[3] *= units[unit] / units["kbit"];
@@ -97,7 +98,7 @@ END{
 				if (a[1] != "world" && a[1] != "all")
 					a[1] = "local";
 
-				p = USRXML_userpipe[i]++;
+				p = ++USRXML_userpipe[i];
 
 				# h,userid,pipeid
 				j = i SUBSEP p;

commit 6f62a6949ddf19141aceb809a77c28367728a37c

repipe:  Fix pipe attribues referencing in pipe.awk
Pipe numbering started with 1; pipe data in libusrxml.awk stored in
USXML_userpipe[h,userid,pipeid] where pipeid starts from 0.

This misconting leads to pipe enumeration errors that visible at
runtime as missing pipe for first direction parsed by pipe.awk from
bin/shapers output.

Fixes: 1674b9d0a2df ("repipe: Strip unit suffix and number pipes correctly")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/bin/pipe.awk
@@ -98,12 +98,12 @@ END{
 				if (a[1] != "world" && a[1] != "all")
 					a[1] = "local";
 
-				p = ++USRXML_userpipe[i];
+				p = USRXML_userpipe[i]++;
 
 				# h,userid,pipeid
 				j = i SUBSEP p;
 
-				USRXML_userpipe[j] = p;
+				USRXML_userpipe[j] = p + 1;
 				USRXML_userpipe[j,"zone"] = a[1];
 				USRXML_userpipe[j,"dir"] = a[2];
 				USRXML_userpipe[j,"bw"] = a[3];